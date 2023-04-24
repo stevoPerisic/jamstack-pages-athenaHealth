95ed16380345430b7c90d23d9442d7284a54fd78
Stevo Perisic <stevo@perisicdesigns.com>
2023-04-24 13:17:18 -0400

    added random headshots

--- _data/providers.js
+++ _data/providers.js
@@ -3,9 +3,24 @@
  */
 const fetch = require('cross-fetch');
 
+async function getRandomHeadshot(num) {
+    const response = await fetch(`https://randomuser.me/api/?results=${num}`);
+    const data = await response.json();
+    const headshots = data.results.map(user => user.picture.large);
+    return headshots;
+}
+
 module.exports = async function () {
     const response = await fetch("https://jamstack.perisicdesigns.workers.dev/providers");
     const data = await response.json();
-    return data.providers.filter(provider => provider.entitytype === "Person");
+    const providers = data.providers.filter(provider => provider.entitytype === "Person");
+    const headshots = await getRandomHeadshot(providers.length);
+    const providersWithHeadshots = providers.map((provider, index) => {
+        return {
+            ...provider,
+            headshotUrl: headshots[index % headshots.length],
+        };
+    });
+    return providersWithHeadshots;
 };
 
