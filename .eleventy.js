5569356676bfbb56b1e6e06ca03ba406453675cd
Stevo Perisic <stevo@perisicdesigns.com>
2023-04-24 10:29:53 -0400

    added the eleventy JS file

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+    return {
+        dir: {
+            input: ".", // The root directory for your Eleventy files
+            includes: "_includes", // The directory where your includes (like 'base.njk') are located
+            // ... other configuration properties
+        },
+    };
+};
