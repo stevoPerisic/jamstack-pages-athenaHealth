18d7bb097f9adceac6e2b6bb442d06e6566f5568
Stevo Perisic <stevo@perisicdesigns.com>
2023-04-29 14:44:17 -0400

    * cleaned up the UI a bit for a nice presentation

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+
     return {
         dir: {
             input: ".", // The root directory for your Eleventy files
